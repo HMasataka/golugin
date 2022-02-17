551bbf23cf7fa056da80fca3fe9a2f469db63af9
Masataka Hisasue <masataka.hisasue@knocknote.co.jp>
2022-02-17 17:46:33 +0900

    Feat how to get plugin file

--- main.go
+++ main.go
@@ -1,17 +1,40 @@
 package main
 
-import "plugin"
+import (
+	"errors"
+	"path/filepath"
+	"plugin"
+)
+
+const (
+	pluginFileName = "plugin.so"
+	pluginDirName  = "so"
+)
 
 func main() {
-	p, err := plugin.Open("plugin.so")
+	pp, err := getPluginFile()
 	if err != nil {
 		panic(err)
 	}
 
-	f, err := p.Lookup("Print")
+	f, err := pp.Lookup("Print")
 	if err != nil {
 		panic(err)
 	}
 
 	f.(func(string))("Hello, World")
+}
+
+func getPluginFile() (*plugin.Plugin, error) {
+	p, err := plugin.Open(pluginFileName)
+	if err == nil {
+		return p, nil
+	}
+
+	p, err = plugin.Open(filepath.Join(pluginDirName, pluginFileName))
+	if err == nil {
+		return p, nil
+	}
+
+	return nil, errors.New("plugin.Open failed")
 }
