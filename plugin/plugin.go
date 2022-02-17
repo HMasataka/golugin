011c4d2790cadd01831a64ca09e562c345826585
Masataka Hisasue <masataka.hisasue@knocknote.co.jp>
2022-02-17 20:19:05 +0900

    Feat output error

--- plugin/plugin.go
+++ plugin/plugin.go
@@ -4,9 +4,12 @@ import "fmt"
 
 func Print(args ...interface{}) {
 	if len(args) != 1 {
+		fmt.Println("error length")
 		return
 	}
 	if v, ok := args[0].(map[string]string); ok {
 		fmt.Println(v)
+		return
 	}
+	fmt.Println("error something")
 }
